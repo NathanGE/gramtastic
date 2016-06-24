17e13d03adfc4a3dab0edc5d09d231f9f48dd03e
NathanGE <nathangelliott@gmail.com>
2016-06-24 14:44:35 -0400

    Added a components directory and Main, PhotoGrid, and Single components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,4 +5,7 @@ import { render } from 'react-dom';
 // Import css
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'));
+// Import Components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
