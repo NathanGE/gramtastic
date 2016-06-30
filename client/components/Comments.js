bffa0a9a07e806285803cdc0cfad8bcb59bdeaf1
NathanGE <nathangelliott@gmail.com>
2016-06-30 15:36:05 -0400

    Made it possible to add and remove comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,18 +7,18 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
   },
   handleSubmit(e) {
     e.preventDefault();
-    console.log("Submitting the form!");
     const { postId } = this.props.params;
     const author = this.refs.author.value;
-    const comments = this.refs.comment.value;
+    const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
   render() {
     return (
